77a87fe7ceef29b8f36e69ae2227b2037a942f66
otmosina <otmosina@gmail.com>
2021-09-18 23:17:14 +0800

    step 5: comples form_for

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -6,8 +6,37 @@ require_relative "hexlet_code/version"
 module HexletCode
   class Error < StandardError; end
 
-  def self.form_for(_model, url: "#")
-    Tag.build("form", action: url, method: "post") {}
+  class Form
+    attr_reader :model
+    def initialize model
+      @model = model
+      @inner_html = ''
+    end
+    def input name, **params
+      value = model.send name
+      @inner_html += case params[:as]
+      when :text
+        Tag.build("textarea", cols:'20', rows:'40', name: name) { value }
+      when :select
+        Tag.build("select", name: name) do
+          params[:collection].inject("") do |result, v|
+             result + if value == v
+              Tag.build("option", value: v, selected:nil) { v }
+            else
+              Tag.build("option", value: v) { v }
+            end
+          end
+        end        
+      else  
+        Tag.build("input", type:'text', value: value, name: name)
+      end
+    end    
+  end
+
+  def self.form_for(model, url: "#")
+    Tag.build("form", action: url, method: "post") do
+      yield(Form.new(model)) if block_given?
+    end 
   end
 
   # Class to generate html tags
@@ -16,7 +45,11 @@ module HexletCode
       html = "<#{name}"
       attrs.each_pair do |k, v|
         html += " "
-        html += "#{k}=\"#{v}\""
+        html += if v.nil?
+          "#{k}"
+        else  
+          "#{k}=\"#{v}\""
+        end
       end
       html += if block_given?
                 ">#{yield}</#{name}>"
--- spec/hexlet_code_spec.rb
+++ spec/hexlet_code_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe HexletCode do
+  User = Struct.new(:name, :job, :gender, keyword_init: true)
   it "has a version number" do
     expect(HexletCode::VERSION).not_to be nil
   end
@@ -14,6 +15,34 @@ RSpec.describe HexletCode do
     it "return correct form" do
       expect(described_class.form_for(user)).to eq('<form action="#" method="post"></form>')
       expect(described_class.form_for(user, url: "/users")).to eq('<form action="/users" method="post"></form>')
+    end
+
+    context 'comples form_for' do
+      let(:user) { User.new name: 'rob', job: 'hexlet', gender: 'm' }
+      let(:form) do
+        HexletCode.form_for user do |f|
+          f.input :name
+          f.input :job, as: :text
+          f.input :gender, as: :select, collection: %w[m f]
+        end
+      end
+      let(:form_html) do
+        html = ""
+        html += '<form action="#" method="post">'
+        html += '<input type="text" value="rob" name="name">'
+        html += '<textarea cols="20" rows="40" name="job">hexlet</textarea>'
+        
+        html += '<select name="gender">'
+        html += '<option value="m" selected>m</option>'
+        html += '<option value="f">f</option>'
+        html += '</select>'      
+        
+        html += '</form>'
+        html
+      end
+      it "return correct form" do
+        expect(form).to eq(form_html)
+      end
     end
   end
 end
