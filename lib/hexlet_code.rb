91d197af5eb629c4833e2242c04cc9f56f4ea50b
otmosina <otmosina@gmail.com>
2021-09-20 11:50:08 +0800

    correct quotes in lib

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-require_relative "hexlet_code/version"
+require_relative 'hexlet_code/version'
 
 # module simple for html generation
 module HexletCode
   class Error < StandardError; end
 
-  def self.form_for(model, url: "#")
-    Tag.build("form", action: url, method: "post") do
+  def self.form_for(model, url: '#')
+    Tag.build('form', action: url, method: 'post') do
       yield(Form.new(model)) if block_given?
     end
   end
@@ -18,19 +18,19 @@ module HexletCode
 
     def initialize(model)
       @model = model
-      @inner_html = ""
+      @inner_html = ''
     end
 
     def input(name, **params)
       value = model.send name
       as, tag_attributes = handle_params(params)
 
-      @inner_html += Tag.build("label", for: name) { name.capitalize }
+      @inner_html += Tag.build('label', for: name) { name.capitalize }
       @inner_html += AsParamMapper.call(as).build(name, value, tag_attributes)
     end
 
-    def submit(value = "Save")
-      @inner_html += Tag.build("input", type: "submit", value: value, name: "commit")
+    def submit(value = 'Save')
+      @inner_html += Tag.build('input', type: 'submit', value: value, name: 'commit')
     end
 
     private
@@ -46,10 +46,10 @@ module HexletCode
   # class for build input text
   class Textfield
     def self.build(name, value, attritutes = {})
-      tag_attributes = { type: "text", name: name, value: value }
+      tag_attributes = { type: 'text', name: name, value: value }
       tag_attributes.merge!(attritutes)
       tag_attributes.delete(:value) if value.nil?
-      Tag.build("input", **tag_attributes)
+      Tag.build('input', **tag_attributes)
     end
   end
 
@@ -58,21 +58,21 @@ module HexletCode
     def self.build(name, value, attritutes = {})
       tag_attributes = { name: name, cols: 20, rows: 40 }
       tag_attributes.merge!(attritutes)
-      Tag.build("textarea", **tag_attributes) { value }
+      Tag.build('textarea', **tag_attributes) { value }
     end
   end
 
   # class for build input select
   class Select
     def self.build(name, value, attritutes = {})
-      render_options = attritutes[:collection].inject("") do |result, v|
+      render_options = attritutes[:collection].inject('') do |result, v|
         result + if value == v
-                   Tag.build("option", value: v, selected: nil) { v }
+                   Tag.build('option', value: v, selected: nil) { v }
                  else
-                   Tag.build("option", value: v) { v }
+                   Tag.build('option', value: v) { v }
                  end
       end
-      Tag.build("select", name: name) do
+      Tag.build('select', name: name) do
         render_options
       end
     end
@@ -83,10 +83,10 @@ module HexletCode
     def self.build(name, **attrs)
       html = "<#{name}"
       attrs.each_pair do |k, v|
-        html += " "
+        html += ' '
         html += v.nil? ? k.to_s : "#{k}=\"#{v}\""
       end
-      html += block_given? ? ">#{yield}</#{name}>" : ">"
+      html += block_given? ? ">#{yield}</#{name}>" : '>'
     end
   end
 
