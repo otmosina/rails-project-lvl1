9329e7a2a4cc6cbb0738a399b81657e0da2aa711
otmosina <otmosina@gmail.com>
2021-09-20 11:31:06 +0800

    refactoring

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -6,6 +6,12 @@ require_relative "hexlet_code/version"
 module HexletCode
   class Error < StandardError; end
 
+  def self.form_for(model, url: "#")
+    Tag.build("form", action: url, method: "post") do
+      yield(Form.new(model)) if block_given?
+    end
+  end
+
   # class for make form object
   class Form
     attr_reader :model
@@ -17,49 +23,57 @@ module HexletCode
 
     def input(name, **params)
       value = model.send name
-      input_params = params.dup
-      input_params.delete(:as)
+      as, tag_attributes = handle_params(params)
 
       @inner_html += Tag.build("label", for: name) { name.capitalize }
-      @inner_html += case params[:as]
-
-                     when :text
-                       input_params = {
-                         name: name,
-                         cols: 20,
-                         rows: 40
-                       }.merge(input_params)
-
-                       Tag.build("textarea", **input_params) { value }
-                     when :select
-                       Tag.build("select", name: name) do
-                         params[:collection].inject("") do |result, v|
-                           result + if value == v
-                                      Tag.build("option", value: v, selected: nil) { v }
-                                    else
-                                      Tag.build("option", value: v) { v }
-                                    end
-                         end
-                       end
-                     else
-
-                       input_params = { type: "text", name: name, value: value }
-                       input_params.merge!(params)
-                       input_params.delete(:value) if value.nil?
-                       Tag.build("input", **input_params)
-                     end
+      @inner_html += AsParamMapper.call(params[:as]).build(name, value, tag_attributes)
     end
 
     def submit(value = "Save")
       @inner_html += Tag.build("input", type: "submit", value: value, name: "commit")
     end
-  end
 
-  def self.form_for(model, url: "#")
-    Tag.build("form", action: url, method: "post") do
-      yield(Form.new(model)) if block_given?
+    private
+
+    def handle_params params
+      as_param = params[:as]
+      tag_attributes = params.dup
+      tag_attributes.delete(:as)
+      return [as_param, tag_attributes]
     end
   end
+
+  class Textfield
+    def self.build(name, value, attritutes = {})
+      tag_attributes = { type: "text", name: name, value: value }
+      tag_attributes.merge!(attritutes)
+      tag_attributes.delete(:value) if value.nil?
+      Tag.build("input", **tag_attributes)   
+    end
+  end  
+
+  class Textarea
+    def self.build(name, value, attritutes = {})
+      tag_attributes = { name: name, cols: 20, rows: 40}
+      tag_attributes.merge!(attritutes)
+      Tag.build("textarea", **tag_attributes) { value }      
+    end
+  end
+
+  class Select
+    def self.build(name, value, attritutes = {})
+      render_options = attritutes[:collection].inject("") do |result, v|
+        result + if value == v
+          Tag.build("option", value: v, selected: nil) { v }
+        else
+          Tag.build("option", value: v) { v }
+        end
+      end
+      Tag.build("select", name: name) do
+        render_options
+      end
+    end
+  end  
 
   # Class to generate html tags
   class Tag
@@ -80,4 +94,15 @@ module HexletCode
               end
     end
   end
+
+  class AsParamMapper
+    MAP = {
+      text: Textarea,
+      select: Select,
+      nil => Textfield
+    }
+    def self.call as
+      return MAP[as]
+    end
+  end  
 end
