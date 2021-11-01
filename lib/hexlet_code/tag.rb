88388125afcda2b6f06fd638a8b15a736a8bc6af
otmosina <otmosina@gmail.com>
2021-11-01 12:31:55 +0800

    rubocop

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -7,24 +7,23 @@ module HexletCode
       VOID_TAGS = %(area, base, br, col, embed, hr, img, input, link, meta, param, source, track, wbr)
       def build(name, **attrs, &block)
         attrs = prepare_attrs(attrs)
-        unless void_tag?(block)
-          "<#{name}#{render_tag_attrs(attrs)}>"
-        else
+        if void_tag?(block)
           "<#{name}#{render_tag_attrs(attrs)}>#{block.call}</#{name}>"
+        else
+          "<#{name}#{render_tag_attrs(attrs)}>"
         end
       end
 
       private
 
-      def prepare_attrs attrs
+      def prepare_attrs(attrs)
         attrs.delete(:value) unless attrs[:value]
-        attrs = attrs.map { |k, v| %( #{k}="#{v}") }
-        attrs
+        attrs.map { |k, v| %( #{k}="#{v}") }
       end
 
       def void_tag?(block)
         !!block
-        #VOID_TAGS.include? name
+        # VOID_TAGS.include? name
       end
 
       def render_tag_attrs(attrs)
