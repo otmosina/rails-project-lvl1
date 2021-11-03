5dc1aa21c295eb15b1ceaffb43bff4adc9ba4770
otmosina <otmosina@gmail.com>
2021-11-03 14:48:12 +0800

    change paired tag func

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -4,13 +4,12 @@ module HexletCode
   # Class to generate html tags
   class Tag
     class << self
-      # VOID_TAGS = %(area, base, br, col, embed, hr, img, input, link, meta, param, source, track, wbr)
       def build(name, **attrs, &block)
         attrs = prepare_attrs(attrs)
-        if void_tag?(block)
-          "<#{name}#{render_tag_attrs(attrs)}>"
-        else
+        if paired_tag?(block)
           "<#{name}#{render_tag_attrs(attrs)}>#{block.call}</#{name}>"
+        else
+          "<#{name}#{render_tag_attrs(attrs)}>"
         end
       end
 
@@ -21,9 +20,8 @@ module HexletCode
         attrs.map { |k, v| %( #{k}="#{v}") }
       end
 
-      def void_tag?(block)
-        !block
-        # VOID_TAGS.include? name
+      def paired_tag?(block)
+        !!block
       end
 
       def render_tag_attrs(attrs)
